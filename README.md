42cfc398c0aaee1c07eefaed613b65788ccd6fd1
Hondallica <hondallica@gmail.com>
2014-09-03 11:33:48 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Decibel - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Decibel;
+
+# DESCRIPTION
+
+WebService::Decibel is ...
+
+# LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hondallica <hondallica@gmail.com>
---
+++ lib/WebService/Decibel.pm
@@ -0,0 +1,39 @@
+package WebService::Decibel;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Decibel - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Decibel;
+
+=head1 DESCRIPTION
+
+WebService::Decibel is ...
+
+=head1 LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hondallica E<lt>hondallica@gmail.comE<gt>
+
+=cut
+
